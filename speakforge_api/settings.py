253e08cbc5bfe7bbdf48e958b4fd5ceb754b7d5d
Makizz <mhartkhiss.degollacion@cit.edu>
2025-02-24 22:39:19 +0800

    wewewe

--- speakforge_api/settings.py
+++ speakforge_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-developmen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,musical-secondly-goat.ngrok-free.app').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
